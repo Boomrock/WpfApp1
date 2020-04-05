a6c925297fad07e9c22c713e0e4506be8d667933
Boomrock <63188345+Boomrock@users.noreply.github.com>
2020-04-05 17:29:00 +0700

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using WpfApp1.data;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private BindingList<Date> Date1;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Date1 = new BindingList<Date>()
+            {
+                new Date() { Text = "1234", Check = true, Text1 = "awdawd" }
+       
+            };
+            dg.ItemsSource = Date1;
+           // Date1.ListChanged += 
+        }
+
+    }
+}
---
+++ data/Date.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.data
+{
+    class Date { 
+
+
+        public bool Check { get; set; }
+        public string Text { get; set; }
+        public string Text1;
+        private string ready;
+        public string Ready
+        {
+            get
+            {
+                if (Check) { return "готово"; }
+                else { return ready; }
+            }
+            set
+            {
+                ready = value;
+            }
+        }
+        public int Number { get; set; }
+    }
+}
